83ecc8f38aa4446de49c393a1beeaf93ef9f4987
Eazymov <eazymov@mail.ru>
2019-02-17 21:20:21 +0500

    add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+/* @flow strict */
+
+const config = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:flowtype/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['flowtype'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    semi: ['error', 'always'],
+  },
+};
+
+module.exports = config;
