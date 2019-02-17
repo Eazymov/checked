fbbf91eed5d31620a3fa671c46ff3a91a6b5645c
Eazymov <eazymov@mail.ru>
2019-02-17 21:15:25 +0500

    add rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+/* @flow */
+import babel from 'rollup-plugin-babel';
+import { uglify } from 'rollup-plugin-uglify';
+import resolve from 'rollup-plugin-node-resolve';
+
+import pkg from './package.json';
+
+const devDependencies = Object.keys(pkg.devDependencies || {});
+
+/* prettier-ignore */
+const banner =
+  '/*!\n' +
+  ' * ' + pkg.name + ' v' + pkg.version + '\n' +
+  ' * (c) 2019 - Present, ' + pkg.author.name + '\n' +
+  ' * Released under the ' + pkg.license + ' License.\n' +
+  ' */';
+
+const config = {
+  input: 'src/index.js',
+  plugins: [
+    babel(),
+    resolve({
+      extensions: ['.js'],
+    }),
+    uglify({
+      output: {
+        comments: (_, { value }) => value.includes('License'),
+      },
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false,
+      },
+    }),
+  ],
+  output: {
+    file: 'dist/index.min.js',
+    format: 'iife',
+    name: 'TypeChecked',
+    banner,
+  },
+  external: [...devDependencies],
+};
+
+export default config;
