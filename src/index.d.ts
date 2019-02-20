e27451f45a20713dcb36eeaa65c73bd29c736deb
Eazymov <eazymov@mail.ru>
2019-02-21 00:18:02 +0500

    fix: add re-exports for isFunc & isUndef

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,5 @@
 export { default as isDef } from './isDef';
+export { default as isFunc } from './isFunc';
+export { default as isUndef } from './isUndef';
 export { default as isString } from './isString';
 export { default as isNumber } from './isNumber';
